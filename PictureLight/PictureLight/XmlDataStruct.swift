01f14e2636240561086171fc7ea6702affe67a80
unromanticman <umworksite@gmail.com>
2016-01-06 00:29:15 +0800

    Ver.07 Patch By UM - add XML Paser and Reader

---
+++ PictureLight/PictureLight/XmlDataStruct.swift
@@ -0,0 +1,47 @@
+//
+//  XmlDataStruct.swift
+//  PictureLight
+//
+//  Created by um on 2016/1/5.
+//  Copyright © 2016年 um. All rights reserved.
+//
+
+import UIKit
+
+class PictureLightData  {
+    
+    var tag:String = ""
+    var fileName:String = ""
+    var fileContent:String = ""
+    init(){
+    }
+    init(t:String,fn:String,fc:String){
+        
+        self.setTag(t).setFileName(fn).setFileContent(fc)
+        
+    }
+    func setTag(tag:String) -> Self
+    {
+        self.tag = tag
+        return self
+    }
+    
+    func setFileName(fn:String) -> Self
+    {
+        self.fileName = fn
+        return self
+    }
+    
+    func setFileContent(fc:String) -> Self
+    {
+        self.fileContent = fc
+        return self
+    }
+    
+    func getTag() -> String{ return  self.tag }
+    
+    func getFileName() -> String{ return self.fileName }
+    
+    func getFileContent() -> String{  return self.fileContent }
+    
+}
